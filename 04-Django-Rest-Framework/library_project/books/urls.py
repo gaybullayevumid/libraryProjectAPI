6117a32a75777be5bfb3a2b78cf3685751755af3
Umid G'aybullayev <109409508+gaybullayevumid@users.noreply.github.com>
2024-10-26 16:36:10 +0500

    feat: create list view using function

--- 04-Django-Rest-Framework/library_project/books/urls.py
+++ 04-Django-Rest-Framework/library_project/books/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import BookListAPIView
+from .views import BookListAPIView, book_list_view
 
 urlpatterns = [
     path('', BookListAPIView.as_view()),
+    path('books/', book_list_view)
 ]
--- 04-Django-Rest-Framework/library_project/books/views.py
+++ 04-Django-Rest-Framework/library_project/books/views.py
@@ -1,3 +1,6 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
@@ -7,3 +10,10 @@ from rest_framework import generics
 class BookListAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+@api_view(['GET'])
+def book_list_view(request, *args, **kwargs):
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data)
