4b9e9ba0b483fc0c5ab66e19f1ca12e87bf461e1
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-01-04 10:57:38 +0500

    feat: validation and FieldValidation

--- 04-Django-Rest-Framework/library_project/books/serializers.py
+++ 04-Django-Rest-Framework/library_project/books/serializers.py
@@ -1,13 +1,51 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
-    title = serializers.CharField(max_length=150)
+    # title = serializers.CharField(max_length=150)
 
 
     class Meta:
         model = Book
         fields = ('id', 'title', 'content', 'subtitle', 'author', 'isbn', 'price',)
+
+    def validate(self, data):
+        title = data.get('title', None)
+        author = data.get('author', None)
+
+        # check title if it contains only alphabetical chars
+        if not title.isalpha():
+            raise ValidationError(
+                {
+                    "status": False,
+                    "message": "Kitobning sarlavhasi harflardan tashkil topgan bo'lishi kerak!"
+                }
+            )
+
+
+        # check title and author from databse existance
+        if Book.objects.filter(title=title, author=author).exists():
+            raise ValidationError(
+                {
+                    "status": False,
+                    "message": "Kitob sarlavhasi va muallifi bir xil bo'lgan kitobni yuklay olmaysiz"
+                }
+            )
+
+
+        return data
+
+    def validate_price(self, price):
+        if price < 0 or price > 9999999999:
+            raise ValidationError(
+                {
+                    "status": False,
+                    "message": "Narx noto'g'ri kiritilgan"
+                }
+            )
+
 
 
 # class BookSerializer(serializers.Serializer):
@@ -17,6 +55,6 @@ class BookSerializer(serializers.ModelSerializer):
 #     author = serializers.CharField()
 
 
-class CashSerializer(serializers.Serializer):
-    input = serializers.CharField(max_length=150)
-    output = serializers.CharField(max_length=120)
+# class CashSerializer(serializers.Serializer):
+#     input = serializers.CharField(max_length=150)
+#     output = serializers.CharField(max_length=120)
