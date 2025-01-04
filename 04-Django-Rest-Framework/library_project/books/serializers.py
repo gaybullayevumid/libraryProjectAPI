e801bf4cc5bff2da96b23e4416e4b386ccad1b0a
gaybullayevumid <umidgaybullayev955@gmail.com>
2025-01-04 10:27:10 +0500

    feat: difference of serializer

--- 04-Django-Rest-Framework/library_project/books/serializers.py
+++ 04-Django-Rest-Framework/library_project/books/serializers.py
@@ -2,6 +2,21 @@ from rest_framework import serializers
 from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    title = serializers.CharField(max_length=150)
+
+
     class Meta:
         model = Book
         fields = ('id', 'title', 'content', 'subtitle', 'author', 'isbn', 'price',)
+
+
+# class BookSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=200)
+#     content = serializers.CharField()
+#     subtitle = serializers.CharField()
+#     author = serializers.CharField()
+
+
+class CashSerializer(serializers.Serializer):
+    input = serializers.CharField(max_length=150)
+    output = serializers.CharField(max_length=120)
